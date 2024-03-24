ee31b38750b6657ce66ea173e25c8632245cb54c
ando_kazuki <azarasikazuki@gmail.com>
2024-03-25 02:41:04 +0900

    save

--- common/src/main/kotlin/jp/inaba/common/domain/shared/PagingCondition.kt
+++ common/src/main/kotlin/jp/inaba/common/domain/shared/PagingCondition.kt
@@ -4,6 +4,14 @@ data class PagingCondition(
     val pageSize: Int,
     val pageNumber: Int,
 ){
+    init {
+        if(pageSize  <= 0) {
+            throw Exception("値がおかしいよ")
+        }
+        if(pageNumber < 0) {
+            throw Exception("値がおかしいよ")
+        }
+    }
     val offset
         get() = pageNumber * pageSize
 }
