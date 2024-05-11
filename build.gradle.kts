09a362b347831ea727b18ac6c5e27b630f8beb71
keloud <12002158+keloud@users.noreply.github.com>
2024-05-11 13:21:51 +0900

    Gradle構成にKtlintを追加 #5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,57 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
-	kotlin("plugin.jpa") version "1.9.22"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
-	targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 allprojects {
-	val axonVersion = "4.9.3"
+    val axonVersion = "4.9.3"
 
-	group = "inaba"
-	version = "0.0.1-SNAPSHOT"
+    group = "inaba"
+    version = "0.0.1-SNAPSHOT"
 
-	apply(plugin = "kotlin")
+    apply(plugin = "kotlin")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 
-	tasks.withType<KotlinCompile> {
-		kotlinOptions {
-			freeCompilerArgs += "-Xjsr305=strict"
-			jvmTarget = "21"
-		}
-	}
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "21"
+        }
+    }
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 
-	dependencies {
-		implementation(platform("org.axonframework:axon-bom:${axonVersion}"))
-		implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
-		implementation("com.michael-bull.kotlin-result:kotlin-result:2.0.0")
-	}
+    dependencies {
+        implementation(platform("org.axonframework:axon-bom:$axonVersion"))
+        implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+        implementation("com.michael-bull.kotlin-result:kotlin-result:2.0.0")
+    }
+
+    // Ktlintの設定値
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        // testディレクトリをコーディング規約チェックから除外する
+        filter {
+            exclude { element ->
+                element.file.path.contains("test")
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,16 +2,12 @@ rootProject.name = "inaba"
 
 include(
     "common",
-
     "catalog:catalog-api",
     "catalog:catalog-service",
-
     "identity:identity-api",
     "identity:identity-service",
-
     "order:order-api",
     "order:order-service",
-
     "basket:basket-api",
-    "basket:basket-service"
+    "basket:basket-service",
 )
