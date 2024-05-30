1ec119ad22e037e165d456b6e77ed5e35451ce9d
ando_kazuki <azarasikazuki@gmail.com>
2024-05-31 01:00:08 +0900

    ktlint

--- identity/identity-service/src/main/kotlin/jp/inaba/identity/service/application/saga/usersetup/UserSetupSaga.kt
+++ identity/identity-service/src/main/kotlin/jp/inaba/identity/service/application/saga/usersetup/UserSetupSaga.kt
@@ -14,6 +14,7 @@ import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.modelling.saga.EndSaga
 import org.axonframework.modelling.saga.MetaDataAssociationResolver
 import org.axonframework.modelling.saga.SagaEventHandler
+import org.axonframework.modelling.saga.SagaLifecycle
 import org.axonframework.modelling.saga.StartSaga
 import org.axonframework.spring.stereotype.Saga
 import org.springframework.beans.factory.annotation.Autowired
