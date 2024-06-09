c06286b4e4e37d58ca45729bc33ad49cac0e3583
ando_kazuki <azarasikazuki@gmail.com>
2024-06-09 17:45:52 +0900

    save

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
     implementation("org.axonframework:axon-modelling")
-    implementation("org.axonframework:axon-server-connector")
     implementation("de.huxhorn.sulky:de.huxhorn.sulky.ulid:8.3.0")
 }
