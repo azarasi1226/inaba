46516bb4e3d0e7e9f6210e449c476f70a7b7e263
ando_kazuki <azarasikazuki@gmail.com>
2024-06-15 02:20:08 +0900

    indexofoooo

--- basket/basket-service/src/test/kotlin/jp/inaba/basket/service/domain/basket/BasketIdTest.kt
+++ basket/basket-service/src/test/kotlin/jp/inaba/basket/service/domain/basket/BasketIdTest.kt
@@ -3,6 +3,7 @@ package jp.inaba.basket.service.domain.basket
 import jp.inaba.basket.api.domain.basket.BasketId
 import jp.inaba.common.domain.shared.DomainException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class BasketIdTest {
@@ -13,11 +14,10 @@ class BasketIdTest {
         }
     }
 
-//    @Test
-//    fun `正常な値でBasketId作成_成功`() {
-//        assertDoesNotThrow {
-//            val userId = UserId()
-//            BasketId("basket-$userId")
-//        }
-//    }
+    @Test
+    fun `正常な値でBasketId作成_成功`() {
+        assertDoesNotThrow {
+            BasketId("01J0BR147P3HTJXDMNZKCCV7Z5")
+        }
+    }
 }
