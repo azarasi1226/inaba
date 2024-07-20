81fb0bcc5ba3c157a1f4b9934fc7b86b7ebdcd1c
ando_kazuki <azarasikazuki@gmail.com>
2024-07-21 02:35:42 +0900

    save

---
+++ datakey/datakey-service/build.gradle.kts
@@ -0,0 +1,23 @@
+apply(plugin = "org.jetbrains.kotlin.jvm")
+apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+apply(plugin = "org.springframework.boot")
+apply(plugin = "io.spring.dependency-management")
+apply(plugin = "kotlin-allopen")
+
+dependencies {
+    implementation(project(":common"))
+
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    // other
+    implementation("software.amazon.awssdk:kms:2.26.21")
+}
