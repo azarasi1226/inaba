207c73b45c40ffa1c40a7dd2c45844d2dbd0fcf7
andoukazuki <seal1226.jp@gmail.com>
2025-01-11 01:22:14 +0900

    savesave

--- inaba/inaba-message/src/main/kotlin/jp/inaba/message/order/command/FailOrderCommand.kt
+++ inaba/inaba-message/src/main/kotlin/jp/inaba/message/order/command/FailOrderCommand.kt
@@ -2,4 +2,6 @@ package jp.inaba.message.order.command
 
 import jp.inaba.core.domain.order.OrderId
 
-data class FailOrderCommand(override val id: OrderId) : OrderAggregateCommand
+data class FailOrderCommand(
+    override val id: OrderId
+) : OrderAggregateCommand
