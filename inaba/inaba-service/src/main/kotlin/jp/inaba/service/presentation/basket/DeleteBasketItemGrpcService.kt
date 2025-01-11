fcde42a494ae95514067b881f49aa23636ce4d6d
andoukazuki <seal1226.jp@gmail.com>
2025-01-11 22:17:12 +0900

    savesave

---
+++ inaba/inaba-service/src/main/kotlin/jp/inaba/service/presentation/basket/DeleteBasketItemGrpcService.kt
@@ -0,0 +1,29 @@
+package jp.inaba.service.presentation.basket
+
+import com.google.protobuf.Empty
+import io.grpc.stub.StreamObserver
+import jp.inaba.core.domain.basket.BasketId
+import jp.inaba.core.domain.product.ProductId
+import jp.inaba.grpc.basket.DeleteBasketItemGrpc
+import jp.inaba.grpc.basket.DeleteBasketItemRequest
+import jp.inaba.message.basket.command.DeleteBasketItemCommand
+import net.devh.boot.grpc.server.service.GrpcService
+import org.axonframework.commandhandling.gateway.CommandGateway
+import jp.inaba.message.basket.deleteBasketItem
+
+@GrpcService
+class DeleteBasketItemGrpcService(
+    private val commandGateway: CommandGateway
+) : DeleteBasketItemGrpc.DeleteBasketItemImplBase() {
+    override fun handle(request: DeleteBasketItemRequest, responseObserver: StreamObserver<Empty>) {
+        val command = DeleteBasketItemCommand(
+            id = BasketId(request.id),
+            productId = ProductId(request.productId)
+        )
+
+        commandGateway.deleteBasketItem(command)
+
+        responseObserver.onNext(Empty.getDefaultInstance())
+        responseObserver.onCompleted()
+    }
+}
