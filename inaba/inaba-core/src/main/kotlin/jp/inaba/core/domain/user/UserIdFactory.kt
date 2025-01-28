7584b93902899a65125d19eeba369151f782b7c0
andoukazuki <seal1226.jp@gmail.com>
2025-01-28 23:13:27 +0900

    save

--- inaba/inaba-core/src/main/kotlin/jp/inaba/core/domain/user/UserIdFactory.kt
+++ inaba/inaba-core/src/main/kotlin/jp/inaba/core/domain/user/UserIdFactory.kt
@@ -1,7 +1,5 @@
 package jp.inaba.core.domain.user
 
-// UserIdはSagaの中でインスタンスを構築します。
-// その際、テストのAssertフェーズでUserIdを特定することができず正確なテストが行えないため、Factoryを用意しています。
 interface UserIdFactory {
     fun handle(): UserId
 }
