c3f29636c53c109463c91dc86e4dc9481a232685
andoukazuki <seal1226.jp@gmail.com>
2025-02-09 18:17:08 +0900

    save

--- inaba/inaba-apigateway/src/main/kotlin/jp/inaba/apigateway/Configuretion.kt
+++ inaba/inaba-apigateway/src/main/kotlin/jp/inaba/apigateway/Configuretion.kt
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
+//TODO(仮でおいてるだけ、後でCORSちゃんと設定しような。)
 @Configuration
 class WebMvcConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
