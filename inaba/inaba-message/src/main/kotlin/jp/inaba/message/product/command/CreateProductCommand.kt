cd3c6f9eeac0b7a3389d32dfd4d6e85efcb9f8c3
andoukazuki <seal1226.jp@gmail.com>
2025-02-10 11:45:48 +0900

    save

--- inaba/inaba-message/src/main/kotlin/jp/inaba/message/product/command/CreateProductCommand.kt
+++ inaba/inaba-message/src/main/kotlin/jp/inaba/message/product/command/CreateProductCommand.kt
@@ -5,11 +5,13 @@ import jp.inaba.core.domain.product.ProductId
 import jp.inaba.core.domain.product.ProductImageURL
 import jp.inaba.core.domain.product.ProductName
 import jp.inaba.core.domain.product.ProductPrice
+import org.axonframework.commandhandling.RoutingKey
 
 data class CreateProductCommand(
-    override val id: ProductId,
+    @RoutingKey
+    val id: ProductId,
     val name: ProductName,
     val description: ProductDescription,
     val imageUrl: ProductImageURL?,
     val price: ProductPrice,
-) : ProductAggregateCommand
+)
