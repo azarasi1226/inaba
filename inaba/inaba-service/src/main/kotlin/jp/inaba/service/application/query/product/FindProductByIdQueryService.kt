a26ab2a84bfea827f64f94a84f40d057dcaf2362
andoukazuki <seal1226.jp@gmail.com>
2025-02-16 14:56:15 +0900

    リファクタ

--- inaba/inaba-service/src/main/kotlin/jp/inaba/service/application/query/product/FindProductByIdQueryService.kt
+++ inaba/inaba-service/src/main/kotlin/jp/inaba/service/application/query/product/FindProductByIdQueryService.kt
@@ -5,31 +5,30 @@ import jp.inaba.core.domain.product.FindProductByIdError
 import jp.inaba.message.product.query.FindProductByIdQuery
 import jp.inaba.message.product.query.FindProductByIdResult
 import jp.inaba.service.infrastructure.jpa.product.ProductJpaRepository
+import jp.inaba.service.infrastructure.jpa.stock.StockJpaRepository
 import org.axonframework.queryhandling.QueryHandler
 import org.springframework.stereotype.Component
 
 @Component
 class FindProductByIdQueryService(
     private val productJpaRepository: ProductJpaRepository,
+    private val stockJpaRepository: StockJpaRepository
 ) {
     @QueryHandler
     fun handle(query: FindProductByIdQuery): FindProductByIdResult {
-        val maybeEntity = productJpaRepository.findById(query.id.value)
-
-        return if (maybeEntity.isEmpty) {
-            throw UseCaseException(FindProductByIdError.PRODUCT_NOT_FOUND)
-        } else {
-            val entity = maybeEntity.get()
-            FindProductByIdResult(
-                id = entity.id,
-                // TODO (nullableだけど、ない場合とかあるか？後で考えよう。)
-                stockId = entity.stockId!!,
-                name = entity.name,
-                description = entity.description,
-                imageUrl = entity.imageUrl,
-                price = entity.price,
-                quantity = entity.quantity,
-            )
+        val productJpaEntity = productJpaRepository.findById(query.id.value).orElseThrow {
+            UseCaseException(FindProductByIdError.PRODUCT_NOT_FOUND)
         }
+        val stockJpaEntity = stockJpaRepository.findByProductId(query.id.value).orElseThrow()
+
+        return FindProductByIdResult(
+            id = productJpaEntity.id,
+            name = productJpaEntity.name,
+            description = productJpaEntity.description,
+            imageUrl = productJpaEntity.imageUrl,
+            price = productJpaEntity.price,
+            stockId = stockJpaEntity.id,
+            quantity = stockJpaEntity.quantity,
+        )
     }
 }
--- inaba/inaba-service/src/main/kotlin/jp/inaba/service/infrastructure/domain/basket/SetBasketItemVerifierImpl.kt
+++ inaba/inaba-service/src/main/kotlin/jp/inaba/service/infrastructure/domain/basket/SetBasketItemVerifierImpl.kt
@@ -4,13 +4,13 @@ import jp.inaba.core.domain.basket.BasketItemQuantity
 import jp.inaba.core.domain.product.ProductId
 import jp.inaba.service.domain.basket.SetBasketItemVerifier
 import jp.inaba.service.infrastructure.jpa.lookupproduct.LookupProductJpaRepository
-import jp.inaba.service.infrastructure.jpa.product.ProductJpaRepository
+import jp.inaba.service.infrastructure.jpa.stock.StockJpaRepository
 import org.springframework.stereotype.Service
 
 @Service
 class SetBasketItemVerifierImpl(
     private val lookupProductJpaRepository: LookupProductJpaRepository,
-    private val productJpaRepository: ProductJpaRepository,
+    private val stockJpaRepository: StockJpaRepository,
 ) : SetBasketItemVerifier {
     override fun isProductNotFound(productId: ProductId): Boolean {
         return !lookupProductJpaRepository.existsById(productId.value)
@@ -20,7 +20,7 @@ class SetBasketItemVerifierImpl(
         productId: ProductId,
         basketItemQuantity: BasketItemQuantity,
     ): Boolean {
-        val entity = productJpaRepository.findById(productId.value).orElseThrow()
+        val entity = stockJpaRepository.findByProductId(productId.value).orElseThrow()
 
         return entity.quantity < basketItemQuantity.value
     }
