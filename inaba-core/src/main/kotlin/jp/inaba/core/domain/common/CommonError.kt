eb5800ec8d5ce95cd5297f2c325e0ca01969711e
andou_kazuki <azarasikazuki@gmail.com>
2026-01-14 22:50:16 +0900

    save

--- inaba-core/src/main/kotlin/jp/inaba/core/domain/common/CommonError.kt
+++ inaba-core/src/main/kotlin/jp/inaba/core/domain/common/CommonError.kt
@@ -4,8 +4,8 @@ enum class CommonError(
     override val errorCode: String,
     override val errorMessage: String,
 ) : UseCaseError {
-    AGGREGATE_DUPLICATED("c1", "集約IDがEventStore内で重複しました"),
+    AGGREGATE_DUPLICATED("c1", "対象のIDを持つ集約がすでに存在します"),
     AGGREGATE_NOT_FOUND("c2", "対象の集約が存在しませんでした"),
     AGGREGATE_DELETED("c3", "すでに削除された集約です"),
-    AGGREGATE_INCOMPATIBLE("c4", "別の種類の集約に対して操作仕様としています"),
+    AGGREGATE_INCOMPATIBLE("c4", "別の種類の集約に対して操作を行おうとしました"),
 }
