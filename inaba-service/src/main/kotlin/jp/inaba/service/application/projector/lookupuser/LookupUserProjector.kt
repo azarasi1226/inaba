fe09729c5c32b8d1a364b0ab6bbe3a67718b5099
andou_kazuki <azarasikazuki@gmail.com>
2026-01-17 13:07:15 +0900

    jooqに書き直し

--- inaba-service/src/main/kotlin/jp/inaba/service/application/projector/lookupuser/LookupUserProjector.kt
+++ inaba-service/src/main/kotlin/jp/inaba/service/application/projector/lookupuser/LookupUserProjector.kt
@@ -3,9 +3,9 @@ package jp.inaba.service.application.projector.lookupuser
 import jp.inaba.message.user.event.UserCreatedEvent
 import jp.inaba.message.user.event.UserDeletedEvent
 import jp.inaba.service.infrastructure.jooq.generated.tables.references.LOOKUP_USER
-import org.jooq.DSLContext
 import org.axonframework.config.ProcessingGroup
 import org.axonframework.eventhandling.EventHandler
+import org.jooq.DSLContext
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,14 +15,15 @@ class LookupUserProjector(
 ) {
     @EventHandler
     fun on(event: UserCreatedEvent) {
-        dsl.insertInto(
-            LOOKUP_USER,
-            LOOKUP_USER.ID,
-            LOOKUP_USER.SUBJECT,
-        ).values(
-            event.id,
-            event.subject,
-        ).execute()
+        dsl
+            .insertInto(
+                LOOKUP_USER,
+                LOOKUP_USER.ID,
+                LOOKUP_USER.SUBJECT,
+            ).values(
+                event.id,
+                event.subject,
+            ).execute()
     }
 
     @EventHandler
