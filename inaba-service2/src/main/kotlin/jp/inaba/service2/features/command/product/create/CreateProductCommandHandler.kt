d8e24bef43a239ab0d7dbd927e9a85bf8065a679
andou_kazuki <azarasikazuki@gmail.com>
2026-02-25 11:56:22 +0900

    save

--- inaba-service2/src/main/kotlin/jp/inaba/service2/features/command/product/create/CreateProductCommandHandler.kt
+++ inaba-service2/src/main/kotlin/jp/inaba/service2/features/command/product/create/CreateProductCommandHandler.kt
@@ -2,14 +2,18 @@ package jp.inaba.service2.features.command.product.create
 
 import jp.inaba.core.domain.product.ProductId
 import jp.inaba.message.InabaEventTag
+import jp.inaba.message.brand.event.BrandCreatedEvent
+import jp.inaba.message.brand.event.BrandDeletedEvent
 import jp.inaba.message.product.command.CreateProductCommand
 import jp.inaba.message.product.command.CreateProductResult
 import jp.inaba.message.product.event.ProductCreatedEvent
+import org.axonframework.eventsourcing.annotation.EventCriteriaBuilder
 import org.axonframework.eventsourcing.annotation.EventSourcingHandler
 import org.axonframework.eventsourcing.annotation.reflection.EntityCreator
 import org.axonframework.extension.spring.stereotype.EventSourced
 import org.axonframework.messaging.commandhandling.annotation.CommandHandler
 import org.axonframework.messaging.eventhandling.gateway.EventAppender
+import org.axonframework.messaging.eventstreaming.EventCriteria
 import org.axonframework.modelling.annotation.InjectEntity
 import org.springframework.stereotype.Component
 
@@ -23,6 +27,10 @@ class CreateProductCommandHandler {
     ): CreateProductResult {
         if (state.created) {
             return CreateProductResult.alreadyExists()
+        }
+
+        if (!state.brandExists) {
+            return CreateProductResult.brandNotFound()
         }
 
         eventAppender.append(
@@ -44,14 +52,32 @@ class CreateProductCommandHandler {
 @EventSourced(tagKey = InabaEventTag.PRODUCT_ID, idType = ProductId::class)
 class State(
     var created: Boolean,
+    var brandExists: Boolean,
 ) {
     @EntityCreator
     constructor() : this(
         created = false,
+        brandExists = false,
     )
+
+    @EventCriteriaBuilder
+    fun buildCriteria(command: CreateProductCommand): EventCriteria =
+        EventCriteria.havingTags(InabaEventTag.PRODUCT_ID, command.id.value)
+            .or()
+            .havingTags(InabaEventTag.BRAND_ID, command.brandId.value)
 
     @EventSourcingHandler
     fun evolve(event: ProductCreatedEvent) {
         created = true
+    }
+
+    @EventSourcingHandler
+    fun evolve(event: BrandCreatedEvent) {
+        brandExists = true
+    }
+
+    @EventSourcingHandler
+    fun evolve(event: BrandDeletedEvent) {
+        brandExists = false
     }
 }
