dff548362c78cc96217bde98f98ecfc4a953ca71
andou_kazuki <azarasikazuki@gmail.com>
2026-02-26 00:02:07 +0900

    save

--- inaba-service2/src/intTest/kotlin/jp/inaba/service2/TestContainerFactory.kt
+++ inaba-service2/src/intTest/kotlin/jp/inaba/service2/TestContainerFactory.kt
@@ -1,11 +1,12 @@
 package jp.inaba.service2
 
+import org.axonframework.test.server.AxonServerContainer
 import org.testcontainers.mysql.MySQLContainer
 import org.testcontainers.utility.MountableFile
 import java.nio.file.Paths
 
-object MySqlTestContainerFactory {
-    fun create(): MySQLContainer =
+object TestContainerFactory {
+    fun mysql(): MySQLContainer =
         MySQLContainer("mysql:8.0").apply {
             // DBの初期化スクリプトのパスを指定
             val hostPath =
@@ -18,4 +19,11 @@ object MySqlTestContainerFactory {
                     .forHostPath(hostPath)
             withCopyFileToContainer(mountable, "/docker-entrypoint-initdb.d/schema.mysql.sql")
         }
+
+    fun axonServer(): AxonServerContainer =
+        AxonServerContainer()
+            .withAxonServerHostname("localhost")
+            .withDcbContext(true)
+            .withReuse(true)
+            .withDevMode(true)
 }
